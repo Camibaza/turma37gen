f018583c03315ca5f1a408429793c355de1f3363
Camibaza <bazante_mila@hotmail.com>
2021-10-28 17:58:34 -0300

    ATIVIDADE 2 - SPRING

---
+++ blogPessoal/blogPessoal/src/main/java/ogr/generation/blogPessoal/Controller/PostagemController.java
@@ -0,0 +1,32 @@
+package ogr.generation.blogPessoal.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ogr.generation.blogPessoal.model.Postagem;
+import ogr.generation.blogPessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+
+public class PostagemController {
+
+	@Autowired
+	private PostagemRepository repository;
+	
+	//metodo
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	
+}
---
+++ blogPessoal/blogPessoal/src/main/java/ogr/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,12 @@
+package ogr.generation.blogPessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ogr.generation.blogPessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+}
